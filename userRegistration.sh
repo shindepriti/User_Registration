6f818bbac11bee92240da68280dc5de799340cad
shindepriti <pritishinde34@gmail.com>
2020-03-17 11:51:15 +0530

    ADDED:Using Regx Validate User Emailid

--- userRegistration.sh
+++ userRegistration.sh
@@ -4,9 +4,11 @@ echo " Wel-Come To User Registration "
 
 read -p "Enter User First Name:-" firstName
 read -p "Enter User Last Name:-" lastName
+read -p "Enter User Email Id:-" emailId
 
 firstNameRegx="^[A-Z][a-zA-Z]{2,}$"
 lastNameRegx="^[A-Z][a-zA-Z]{2,}$"
+emailRegx="^([a-zA-Z]{3,}([._+-]?[a-zA-Z0-9])*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}[.]?[a-zA-Z]*)$"
 
 function validateUser(){
 	if [[	$1 =~ $2    ]]
@@ -18,3 +20,4 @@ function validateUser(){
 }
 validateUser $firstName $firstNameRegx 
 validateUser $lastName $lastNameRegx
+validateUser $emailId $emailRegx
