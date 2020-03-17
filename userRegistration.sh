30519ac6fee19aefb228c78cfb3ca3d3b41325bb
shindepriti <pritishinde34@gmail.com>
2020-03-17 10:46:17 +0530

    ADDED:Using RegX Validate User First Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,3 +2,15 @@
 shopt -s extglob
 echo " Wel-Come To User Registration "
 
+read -p "Enter User First name:-" firstName
+firstNameRegx="^[A-Z][a-zA-Z]{2,}$"
+
+function validateUser(){
+	if [[	$1 =~ $2    ]]
+	then
+		echo "valid "
+	else
+		echo  "Invalid"
+	fi
+}
+validateUser $firstName  $firstNameRegx 
